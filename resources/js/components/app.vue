70b76435999030535796a15177751b7c7b8e9662
George Rabus <george.rabus@isa.utm.md>
2025-02-14 11:40:29 +0200

    route setup

---
+++ resources/js/components/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Hello App!</h1>
+    <p>
+        <strong>Current route path:</strong>{{$route.fullPath}}
+    </p>
+    <nav>
+        <div>
+            <router-link class="link" to="/">Home</router-link>
+            <router-link class="link" to="/about">About</router-link>
+        </div>
+    </nav>
+    <main>
+        <router-view/>
+    </main>
+</template>
